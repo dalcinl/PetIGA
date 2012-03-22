b81f7b0b388a2f3b7ec9b3370d02ef66e6d1cb9e
Lisandro Dalcin <dalcinl@gmail.com>
2012-03-22 16:08:42 +0300

    More work on I/O

---
+++ demo/InputOutput.c
@@ -0,0 +1,56 @@
+#include "petiga.h"
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc, char *argv[]) {
+
+  PetscErrorCode  ierr;
+  ierr = PetscInitialize(&argc,&argv,0,0);CHKERRQ(ierr);
+
+  PetscInt  i;
+  PetscInt  dim = 3;
+  PetscInt  dof = 1;
+  PetscBool b[3] = {PETSC_FALSE, PETSC_FALSE, PETSC_FALSE};
+  PetscInt  N[3] = {16,16,16}; 
+  PetscInt  p[3] = { 2, 2, 2};
+  PetscInt  C[3] = {-1,-1,-1};
+  PetscInt  n0=3, n1=3, n2=3, n3=3;
+  ierr = PetscOptionsBegin(PETSC_COMM_WORLD,"","InputOutput Options","IGA");CHKERRQ(ierr);
+  ierr = PetscOptionsInt("-dim","dimension",__FILE__,dim,&dim,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsInt("-dof","dofs/node",__FILE__,dof,&dof,PETSC_NULL);CHKERRQ(ierr);
+  n0 = n1 = n2 = n3 = dim;
+  ierr = PetscOptionsBoolArray("-periodic","periodicity",     __FILE__,b,&n0,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsIntArray ("-N","number of elements",     __FILE__,N,&n1,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsIntArray ("-p","polynomial order",       __FILE__,p,&n2,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsIntArray ("-C","global continuity order",__FILE__,C,&n3,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsEnd();CHKERRQ(ierr);
+  if (n0<3) b[2] = b[0]; if (n0<2) b[1] = b[0];
+  if (n1<3) N[2] = N[0]; if (n1<2) N[1] = N[0];
+  if (n2<3) p[2] = p[0]; if (n2<2) p[1] = p[0];
+  if (n3<3) C[2] = C[0]; if (n3<2) C[1] = C[0];
+  for (i=0; i<dim; i++)  if (C[i] ==-1) C[i] = p[i] - 1;
+  
+  IGA iga; IGAAxis axis;
+  ierr = IGACreate(PETSC_COMM_WORLD,&iga);CHKERRQ(ierr);
+  ierr = IGASetDim(iga,dim);CHKERRQ(ierr);
+  ierr = IGASetDof(iga,dof);CHKERRQ(ierr);
+  for (i=0; i<dim; i++) {
+    ierr = IGAGetAxis(iga,i,&axis);CHKERRQ(ierr);
+    ierr = IGAAxisSetPeriodic(axis,b[i]);CHKERRQ(ierr);
+    ierr = IGAAxisInitUniform(axis,p[i],C[i],N[i],0.0,1.0);CHKERRQ(ierr);
+  }
+  ierr = IGASetUp(iga);CHKERRQ(ierr);
+  
+  MPI_Comm comm;
+  ierr = IGAGetComm(iga,&comm);CHKERRQ(ierr);
+  ierr = IGAWrite(iga,"iga.dat");CHKERRQ(ierr);
+  IGA iganew;
+  ierr = IGACreate(comm,&iganew);CHKERRQ(ierr);
+  ierr = IGARead(iganew,"iga.dat");CHKERRQ(ierr);
+  ierr = IGASetUp(iganew);CHKERRQ(ierr);
+
+  ierr = IGADestroy(&iganew);CHKERRQ(ierr);
+  ierr = IGADestroy(&iga);CHKERRQ(ierr);
+  ierr = PetscFinalize();CHKERRQ(ierr);
+  return 0;
+ }
--- src/petigaio.c
+++ src/petigaio.c
@@ -7,24 +7,31 @@
 PetscErrorCode IGALoad(IGA iga,PetscViewer viewer)
 {
   PetscBool      isbinary;
+  PetscBool      skipheader;
   PetscErrorCode ierr;
   PetscFunctionBegin;
   PetscValidHeaderSpecific(iga,IGA_CLASSID,1);
   PetscValidHeaderSpecific(viewer,PETSC_VIEWER_CLASSID,2);
   PetscCheckSameComm(iga,1,viewer,2);
+
+  /* */
   ierr = PetscTypeCompare((PetscObject)viewer,PETSCVIEWERBINARY,&isbinary);CHKERRQ(ierr);
   if (!isbinary) SETERRQ(((PetscObject)viewer)->comm,PETSC_ERR_ARG_WRONG,"Only for binary viewers");
+  ierr = PetscViewerBinaryGetSkipHeader(viewer,&skipheader);CHKERRQ(ierr);
+
   { /* */
-    PetscInt i,buf[3];
-    PetscInt classid,dim;
-    ierr = PetscViewerBinaryRead(viewer,buf,2,PETSC_INT);CHKERRQ(ierr);
-    classid = buf[0]; dim = buf[1];
-    if (classid != IGA_FILE_CLASSID) SETERRQ(((PetscObject)viewer)->comm,PETSC_ERR_ARG_WRONG,"Not an IGA in file");
+    PetscInt i,dim;
+    if (!skipheader) {
+      PetscInt classid;
+      ierr = PetscViewerBinaryRead(viewer,&classid,1,PETSC_INT);CHKERRQ(ierr);
+      if (classid != IGA_FILE_CLASSID) SETERRQ(((PetscObject)viewer)->comm,PETSC_ERR_ARG_WRONG,"Not an IGA in file");
+    }
+    ierr = PetscViewerBinaryRead(viewer,&dim,1,PETSC_INT);CHKERRQ(ierr);
     ierr = IGASetDim(iga,dim);CHKERRQ(ierr);
     for (i=0; i<dim; i++) {
       IGAAxis   axis;
       PetscBool periodic;
-      PetscInt  p,n,m;
+      PetscInt  p,n,m,buf[3];
       PetscReal *U;
       ierr = PetscViewerBinaryRead(viewer,&buf,3,PETSC_INT);CHKERRQ(ierr);
       periodic = buf[0] ? PETSC_TRUE : PETSC_FALSE;
@@ -34,7 +41,7 @@ PetscErrorCode IGALoad(IGA iga,PetscViewer viewer)
       ierr = IGAAxisSetOrder(axis,p);CHKERRQ(ierr);CHKERRQ(ierr);
       ierr = IGAAxisSetKnots(axis,m,0);CHKERRQ(ierr);CHKERRQ(ierr);
       ierr = IGAAxisGetKnots(axis,0,&U);CHKERRQ(ierr);CHKERRQ(ierr);
-      ierr = PetscViewerBinaryRead(viewer,&U,m,PETSC_REAL);CHKERRQ(ierr);
+      ierr = PetscViewerBinaryRead(viewer,U,m+1,PETSC_REAL);CHKERRQ(ierr);
     }
   }
   PetscFunctionReturn(0);
@@ -44,17 +51,16 @@ PetscErrorCode IGALoad(IGA iga,PetscViewer viewer)
 #define __FUNCT__ "IGASave"
 PetscErrorCode IGASave(IGA iga,PetscViewer viewer)
 {
+  PetscBool      isbinary;
+  PetscBool      skipheader;
   PetscErrorCode ierr;
   PetscFunctionBegin;
   PetscValidHeaderSpecific(iga,IGA_CLASSID,1);
 
   /* */
   if (viewer) {
-    PetscBool isbinary;
     PetscValidHeaderSpecific(viewer,PETSC_VIEWER_CLASSID,2);
     PetscCheckSameComm(iga,1,viewer,2);
-    ierr = PetscTypeCompare((PetscObject)viewer,PETSCVIEWERBINARY,&isbinary);CHKERRQ(ierr);
-    if (!isbinary) SETERRQ(((PetscObject)viewer)->comm,PETSC_ERR_ARG_WRONG,"Only for binary viewers");
   } else {
     MPI_Comm comm;
     ierr = IGAGetComm(iga,&comm);CHKERRQ(ierr);
@@ -62,24 +68,33 @@ PetscErrorCode IGASave(IGA iga,PetscViewer viewer)
     if (!viewer) PetscFunctionReturn(PETSC_ERR_PLIB);
   }
 
+  /* */
+  ierr = PetscTypeCompare((PetscObject)viewer,PETSCVIEWERBINARY,&isbinary);CHKERRQ(ierr);
+  if (!isbinary) SETERRQ(((PetscObject)viewer)->comm,PETSC_ERR_ARG_WRONG,"Only for binary viewers");
+  ierr = PetscViewerBinaryGetSkipHeader(viewer,&skipheader);CHKERRQ(ierr);
+
   { /* */
-    PetscInt i,buf[3];
-    PetscInt dim;
-    ierr = IGAGetDim(iga,&dim);
-    buf[0] = IGA_FILE_CLASSID; buf[1] = dim;
-    ierr = PetscViewerBinaryWrite(viewer,buf,2,PETSC_INT,PETSC_TRUE);CHKERRQ(ierr);
+    PetscInt i,dim;
+    ierr = IGAGetDim(iga,&dim);CHKERRQ(ierr);
+    if (!skipheader) {
+      PetscInt classid = IGA_FILE_CLASSID;
+      ierr = PetscViewerBinaryWrite(viewer,&classid,1,PETSC_INT,PETSC_TRUE);CHKERRQ(ierr);
+    }
+    ierr = PetscViewerBinaryWrite(viewer,&dim,1,PETSC_INT,PETSC_FALSE);CHKERRQ(ierr);
     for (i=0; i<dim; i++) {
       IGAAxis   axis;
       PetscBool periodic;
-      PetscInt  p,n,m;
+      PetscInt  p,n,m,buf[3];
       PetscReal *U;
       ierr = IGAGetAxis(iga,i,&axis);CHKERRQ(ierr);
       ierr = IGAAxisGetPeriodic(axis,&periodic);CHKERRQ(ierr);
       ierr = IGAAxisGetOrder(axis,&p);CHKERRQ(ierr);
       ierr = IGAAxisGetKnots(axis,&m,&U);CHKERRQ(ierr);
-      buf[0] = periodic; buf[1] = p; buf[2] = n = m-p-1;
+      buf[0] = periodic;
+      buf[1] = p;
+      buf[2] = n = m-p-1;
       ierr = PetscViewerBinaryWrite(viewer,buf,3,PETSC_INT,PETSC_TRUE);CHKERRQ(ierr);
-      ierr = PetscViewerBinaryWrite(viewer,U,m,PETSC_REAL,PETSC_FALSE);CHKERRQ(ierr);
+      ierr = PetscViewerBinaryWrite(viewer,U,m+1,PETSC_REAL,PETSC_FALSE);CHKERRQ(ierr);
     }
     ierr = PetscViewerFlush(viewer);CHKERRQ(ierr);
   }
@@ -95,6 +110,7 @@ PetscErrorCode IGARead(IGA iga,const char filename[])
   PetscErrorCode ierr;
   PetscFunctionBegin;
   PetscValidHeaderSpecific(iga,IGA_CLASSID,1);
+  PetscValidCharPointer(filename,2);
   ierr = IGAGetComm(iga,&comm);CHKERRQ(ierr);
   ierr = PetscViewerBinaryOpen(comm,filename,FILE_MODE_READ,&viewer);CHKERRQ(ierr);
   ierr = IGALoad(iga,viewer);CHKERRQ(ierr);
@@ -111,8 +127,13 @@ PetscErrorCode IGAWrite(IGA iga,const char filename[])
   PetscErrorCode ierr;
   PetscFunctionBegin;
   PetscValidHeaderSpecific(iga,IGA_CLASSID,1);
+  PetscValidCharPointer(filename,2);
   ierr = IGAGetComm(iga,&comm);CHKERRQ(ierr);
-  ierr = PetscViewerBinaryOpen(comm,filename,FILE_MODE_WRITE,&viewer);CHKERRQ(ierr);
+  ierr = PetscViewerCreate(comm,&viewer);CHKERRQ(ierr);
+  ierr = PetscViewerSetType(viewer,PETSCVIEWERBINARY);CHKERRQ(ierr);
+  ierr = PetscViewerBinarySkipInfo(viewer);CHKERRQ(ierr);
+  ierr = PetscViewerFileSetMode(viewer,FILE_MODE_WRITE);CHKERRQ(ierr);
+  ierr = PetscViewerFileSetName(viewer,filename);CHKERRQ(ierr);
   ierr = IGASave(iga,viewer);CHKERRQ(ierr);
   ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
   PetscFunctionReturn(0);
