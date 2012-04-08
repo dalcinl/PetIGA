d4455dead348d5532f61cca724ec16d823bf122f
Lisandro Dalcin <dalcinl@gmail.com>
2012-04-08 16:01:25 +0300

    Fixes and change of boundary conditions for the Laplace demo

--- demo/Laplace.c
+++ demo/Laplace.c
@@ -4,12 +4,11 @@
 #define __FUNCT__ "System"
 PetscErrorCode System(IGAPoint p,PetscScalar *K,PetscScalar *F,void *ctx)
 {
-  PetscInt  nen,dim;
+  PetscInt nen,dim;
   IGAPointGetSizes(p,&nen,0,&dim);
 
-  const PetscReal *N0,*N1;
-  IGAPointGetShapeFuns(p,0,&N0);
-  IGAPointGetShapeFuns(p,0,&N1);
+  const PetscReal *N1;
+  IGAPointGetShapeFuns(p,1,&N1);
 
   PetscInt a,b,i;
   for (a=0; a<nen; a++) {
@@ -31,23 +30,20 @@ int main(int argc, char *argv[]) {
   PetscErrorCode ierr;
   ierr = PetscInitialize(&argc,&argv,0,0);CHKERRQ(ierr);
 
-  PetscInt  i,j;
+  PetscInt  i;
   PetscInt  dim = 3;
   PetscInt  dof = 1;
-  PetscBool t[3] = {PETSC_FALSE, PETSC_FALSE, PETSC_FALSE};
   PetscInt  N[3] = {16,16,16};
   PetscInt  p[3] = { 2, 2, 2};
   PetscInt  C[3] = {-1,-1,-1};
-  PetscInt  n0=3, n1=3, n2=3, n3=3;
+  PetscInt  n1=3, n2=3, n3=3;
   ierr = PetscOptionsBegin(PETSC_COMM_WORLD,"","Laplace Options","IGA");CHKERRQ(ierr);
   ierr = PetscOptionsInt("-dim","dimension",__FILE__,dim,&dim,PETSC_NULL);CHKERRQ(ierr);
-  n0 = n1 = n2 = n3 = dim;
-  ierr = PetscOptionsBoolArray("-periodic","periodicity",     __FILE__,t,&n0,PETSC_NULL);CHKERRQ(ierr);
+  n1 = n2 = n3 = dim;
   ierr = PetscOptionsIntArray ("-N","number of elements",     __FILE__,N,&n1,PETSC_NULL);CHKERRQ(ierr);
   ierr = PetscOptionsIntArray ("-p","polynomial order",       __FILE__,p,&n2,PETSC_NULL);CHKERRQ(ierr);
   ierr = PetscOptionsIntArray ("-C","global continuity order",__FILE__,C,&n3,PETSC_NULL);CHKERRQ(ierr);
   ierr = PetscOptionsEnd();CHKERRQ(ierr);
-  if (n0<3) t[2] = t[0]; if (n0<2) t[1] = t[0];
   if (n1<3) N[2] = N[0]; if (n1<2) N[1] = N[0];
   if (n2<3) p[2] = p[0]; if (n2<2) p[1] = p[0];
   if (n3<3) C[2] = C[0]; if (n3<2) C[1] = C[0];
@@ -60,16 +56,11 @@ int main(int argc, char *argv[]) {
   for (i=0; i<dim; i++) {
     IGAAxis axis;
     ierr = IGAGetAxis(iga,i,&axis);CHKERRQ(ierr);
-    ierr = IGAAxisSetPeriodic(axis,t[i]);CHKERRQ(ierr);
     ierr = IGAAxisSetDegree(axis,p[i]);CHKERRQ(ierr);
     ierr = IGAAxisInitUniform(axis,N[i],0.0,1.0,C[i]);CHKERRQ(ierr);
-    for (j=0; j<2; j++) {
-      IGABoundary bnd;
-      PetscInt    field = 0;
-      PetscScalar value = j ? 1.0 : 0.0;
-      ierr = IGAGetBoundary(iga,i,j,&bnd);CHKERRQ(ierr);
-      ierr = IGABoundarySetValue(bnd,field,value);CHKERRQ(ierr);
-    }
+    IGABoundary bnd;
+    ierr = IGAGetBoundary(iga,i,0,&bnd);CHKERRQ(ierr);
+    ierr = IGABoundarySetValue(bnd,0,1.0);CHKERRQ(ierr);
   }
   ierr = IGASetFromOptions(iga);CHKERRQ(ierr);
   ierr = IGASetUp(iga);CHKERRQ(ierr);
@@ -80,6 +71,7 @@ int main(int argc, char *argv[]) {
   ierr = IGACreateVec(iga,&x);CHKERRQ(ierr);
   ierr = IGACreateVec(iga,&b);CHKERRQ(ierr);
   ierr = IGAFormSystem(iga,A,b,System,PETSC_NULL);CHKERRQ(ierr);
+  ierr = MatSetOption(A,MAT_SYMMETRIC,PETSC_TRUE);CHKERRQ(ierr);
 
   KSP ksp;
   ierr = IGACreateKSP(iga,&ksp);CHKERRQ(ierr);
