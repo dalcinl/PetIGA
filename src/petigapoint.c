eb67ef173c90c9c537073bf2b9d6f38ec3afa953
Lisandro Dalcin <dalcinl@gmail.com>
2012-04-19 18:46:17 +0300

    Fix signed/unsigned comparison

--- src/petigapoint.c
+++ src/petigapoint.c
@@ -204,7 +204,7 @@ PetscErrorCode IGAPointGetShapeFuns(IGAPoint point,PetscInt der,const PetscReal 
   PetscFunctionBegin;
   PetscValidPointer(point,1);
   PetscValidPointer(shapefuns,2);
-  if (PetscUnlikely(der < 0 || der >= sizeof(point->shape)/sizeof(PetscReal*)))
+  if (PetscUnlikely(der < 0 || der >= (PetscInt)(sizeof(point->shape)/sizeof(PetscReal*))))
     SETERRQ2(PETSC_COMM_SELF,PETSC_ERR_ARG_OUTOFRANGE,
             "Requested derivative must be in range [0,%d], got %D",
             (int)(sizeof(point->shape)/sizeof(PetscReal*)-1),der);
